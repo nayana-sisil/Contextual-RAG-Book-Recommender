b8ce1e433b24e032581f62c6b4f6c2560b3a9441
nayana sisil <nayanasisil@gmail.com>
2026-04-05 11:58:10 +0530

    uppdatig rag agent

---
+++ rag_agent/rag_agent.py
@@ -0,0 +1,18 @@
+import os
+import re
+import json
+import pandas as pd
+import numpy as np
+from typing import Optional
+from dotenv import load_dotenv
+ 
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain.tools import Tool
+from langchain.prompts import PromptTemplate
+from langchain_community.document_loaders import TextLoader
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+ 
+from observability import setup_langsmith, RunTracker
+from reranker import BookReranker
+from llm_local import get_llm, EXPLAIN_PROMPT, QUERY_ANALYSIS_PROMPT
